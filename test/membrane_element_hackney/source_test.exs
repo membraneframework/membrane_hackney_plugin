a7bdb278d6b308fea049ca8362a7ddeef469ccb7
Michał Śledź <michal.sledz@swmansion.com>
2020-09-23 17:20:34 +0200

    Update membrane-core version. Bump to 0.4.0 version

--- test/membrane_element_hackney/source_test.exs
+++ test/membrane_element_hackney/source_test.exs
@@ -21,7 +21,13 @@ defmodule Membrane.Element.Hackney.SourceTest do
     pos_counter: 0
   }
 
-  @ctx_other_pl %Ctx.Other{playback_state: :playing, pads: %{}, clock: nil, parent_clock: nil}
+  @ctx_other_pl %Ctx.Other{
+    playback_state: :playing,
+    pads: %{},
+    clock: nil,
+    parent_clock: nil,
+    name: :source
+  }
 
   def state_streaming(_) do
     state =
