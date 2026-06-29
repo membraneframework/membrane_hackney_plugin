176d2cf3f7a225b5763e1749594af98f757680af
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:26:56 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Membrane.Hackney.Plugin.Mixfile do
       {:membrane_core, "~> 1.0"},
       {:hackney, "~> 1.16"},
       {:mockery, "~> 2.3", runtime: false},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
